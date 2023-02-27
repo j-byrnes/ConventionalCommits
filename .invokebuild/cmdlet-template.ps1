2b7c3f39cc82dbd7db90a04025a375f5436100af
Josh Byrnes <jpbyrnes@multicare.org>
2023-02-26 19:26:38 -0800

    chore: Updated house-keeping files

---
+++ .invokebuild/cmdlet-template.ps1
@@ -0,0 +1,43 @@
+<#
+  .SYNOPSIS
+  Template Section
+
+  .DESCRIPTION
+  Template Section
+
+  .PARAMETER <ParameterName>
+  Template Section
+
+  .EXAMPLE
+  Template Section
+
+  .INPUTS
+  Template Section
+
+  .OUTPUTS
+  Template Section
+
+  .NOTES
+  Template Section
+
+  .LINK
+  Template Section
+#>
+function cmdlet-template {
+  [CmdletBinding()]
+  param (
+    
+  )
+
+  begin {
+    
+  }
+
+  process {
+    
+  }
+
+  end {
+    
+  }
+}
